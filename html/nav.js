d69448af4c4fb3376d817928672a46de25fa1938
jgtaft <jgt43@cornell.edu>
2018-06-07 12:13:46 -0400

    navigation in separate file

---
+++ html/nav.js
@@ -0,0 +1,26 @@
+document.getElementById("topNav").innerHTML=
+'<div class="topnav">' +
+  '<div id="logoCont">' +
+  '<a id="logo" href="#testdrive">Social Media TestDrive</a>' +
+  '</div>' +
+  '<div id="linksCont">' +
+    '<a class="links" href="html/lessons.html"><p>Lessons</p></a>'+
+    '<a class="links" href="html/for_parents.html"><p>For Parents</p></a>'+
+    '<a class="links" href="html/about_us.html"><p>About</p></a>'+
+    '<a class="links" href="html/team.html"><p>The Team</p></a>'+
+    '<a class="links" href="html/gettingstarted.html"><p>Get Started</p><a>'+
+  '</div>'+
+'</div>';
+
+document.getElementById("bottomNav").innerHTML=
+'<div class="footer">' +
+  '<div class="footCont_title"><a class="fTL" href="../index.html"><p class="footertitle">Social Media</p><p class="footertitle">TestDrive</p><a></div>' +
+  '<div class="footCont_links">'+
+  '<a class="footerlinks" href="lessons.html"><p>Lessons</p></a>'+
+  '<a class="footerlinks" href="for_parents.html"><p>For Parents</p></a>'+
+  '<a class="footerlinks" href="about_us.html"><p>About</p></a>' +
+    '<a class="footerlinks" href="team.html"><p>The Team</p></a>' +
+    '<a class="footerlinks" href="gettingstarted.html"><p>Get Started</p><a>' +
+    '<a class="footerlinks" href="terms.html"><p>Terms & Copyright</p><a>' +
+  '</div>' +
+'</div>';
