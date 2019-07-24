d09cba58d040fb012778057b6c6231786b699c62
Lucy <hl566@cornell.edu>
2019-07-24 17:08:54 -0400

    added 'for educators' page, fixed lesson pages nav bars, made banners less thick, changed Get Started page to FAQ

--- nav.js
+++ nav.js
@@ -6,9 +6,10 @@ document.getElementById("topNav").innerHTML=
   '<div id="linksCont">' +
     '<a class="links" href="about_us.html"><p>About</p></a>'+
     '<a class="links" href="lessons.html"><p>Lessons</p></a>'+
+    '<a class="links" href="for_educators.html"><p>For Educators</p></a>'+
     '<a class="links" href="for_parents.html"><p>For Parents</p></a>'+
     '<a class="links" href="team.html"><p>The Team</p></a>'+
-    '<a class="links" href="gettingstarted.html"><p>Get Started</p><a>'+
+    '<a class="links" href="faq.html"><p>FAQ</p><a>'+
   '</div>'+
 '</div>';
 
@@ -17,9 +18,10 @@ document.getElementById("bottomNav").innerHTML=
   '<div class="footCont_title"><a class="fTL" href="../index.html"><p class="footertitle">Social Media</p><p class="footertitle">TestDrive</p><a></div>' +
   '<div class="footCont_links">'+
     '<a class="footerlinks" href="terms.html"><p>Terms & Copyright</p><a>' +
-    '<a class="footerlinks" href="gettingstarted.html"><p>Get Started</p><a>' +
+    '<a class="footerlinks" href="faq.html"><p>FAQ</p><a>' +
     '<a class="footerlinks" href="team.html"><p>The Team</p></a>' +
     '<a class="footerlinks" href="for_parents.html"><p>For Parents</p></a>'+
+    '<a class="footerlinks" href="for_educators.html"><p>For Educators</p></a>' +
     '<a class="footerlinks" href="lessons.html"><p>Lessons</p></a>'+
     '<a class="footerlinks" href="about_us.html"><p>About</p></a>' +
   '</div>' +
